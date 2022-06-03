2fccd06cd4c613013ce72f33f79a7051c9d9171c
Daniel Morales <dagamo0605@gmail.com>
2022-06-02 22:25:57 -0500

    Sección testimonios / Instalación de SwiperSlide

--- src/components/Testimonios/Testimonios.js
+++ src/components/Testimonios/Testimonios.js
@@ -1,14 +1,67 @@
 import React from "react"
+import {Tarjeta} from "./TestimoniosElements"
+import {Img} from "./TestimoniosElements"
+import {Comentario} from "./TestimoniosElements"
+import {Nombre} from "./TestimoniosElements"
+import {Title} from "./TestimoniosElements"
+
+
+import imagenDaniel from "../../assets/Daniel.jpg"
+import imagenAngel from "../../assets/Angel.jpg"
+import imagenDamian from "../../assets/Damian.jpg"
+import imagenAbner from "../../assets/Abner.jpg"
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "./swiperSlide.css";
+// import required modules
+import { Pagination } from "swiper";
 
 export default function Testimonios() {
-	const styles = {
-		height: '1500px',
-		margin: 0
-	}
 
 	return (
-		<div id="testimonios" style={styles}>
-			<h1 style={{ margin: 0 }} >Testimonios</h1>
-		</div>
+		<div id="testimonios">
+		<Title>Nuestra pasión, tu satisfacción...</Title>
+		<Swiper        
+			pagination={{dynamicBullets: true,}}
+        	modules={[Pagination]}>
+
+		  	<SwiperSlide>
+				<Tarjeta>
+					<Comentario>“Un lugar muy agradable, con muy buenos platillos. Variedad de cortes, ensaladas y hasta pizzas al horno. Hay también desayuno buffet. El personal es muy atento y amable, siempre al pendiente de los clientes. Definitivamente es recomendable”.</Comentario>
+					<Nombre>Daniel García Morales</Nombre>
+				</Tarjeta> 
+				<Img src={imagenDaniel}/>
+		  	</SwiperSlide>
+
+		  	<SwiperSlide>
+				<Tarjeta>
+					<Comentario>“Un lugar muy agradable, con muy buenos platillos. Variedad de cortes, ensaladas y hasta pizzas al horno. Hay también desayuno buffet. El personal es muy atento y amable, siempre al pendiente de los clientes. Definitivamente es recomendable”.</Comentario>
+					<Nombre>Angel de Jesús Sánchez Morales</Nombre>
+					</Tarjeta> 
+				<Img src={imagenAngel}/>
+			</SwiperSlide>
+
+			<SwiperSlide>
+				<Tarjeta>
+					<Comentario>“Un lugar muy agradable, con muy buenos platillos. Variedad de cortes, ensaladas y hasta pizzas al horno. Hay también desayuno buffet. El personal es muy atento y amable, siempre al pendiente de los clientes. Definitivamente es recomendable”.</Comentario>
+					<Nombre>José Damián Mendoza Carmona</Nombre>
+					</Tarjeta> 
+				<Img src={imagenDamian}/>
+			</SwiperSlide>
+
+			<SwiperSlide>
+				<Tarjeta>
+					<Comentario>“Un lugar muy agradable, con muy buenos platillos. Variedad de cortes, ensaladas y hasta pizzas al horno. Hay también desayuno buffet. El personal es muy atento y amable, siempre al pendiente de los clientes. Definitivamente es recomendable”.</Comentario>
+					<Nombre>Abner Jeffrey Tapia Cruz</Nombre>
+				</Tarjeta> 
+				<Img src={imagenAbner}/>
+			</SwiperSlide>
+
+		</Swiper>
+	  </div>
 	)
 }
--- src/components/Testimonios/TestimoniosElements.js
+++ src/components/Testimonios/TestimoniosElements.js
@@ -1,1 +1,38 @@
 import styled from "styled-components"
+
+export const Tarjeta = styled.div`
+    width: 60%;
+    height: 60%;
+    background-color: #222222;
+    border-radius: 5%;
+`
+
+export const Title = styled.h1`
+    color: #000;
+    margin: 5%;
+    text-align:center;
+`
+
+export const Img = styled.img`
+    border-radius: 50%;
+    max-width: 15em;
+	max-height: 15em;
+    position: absolute;
+    top: 0%;
+`
+
+export const Comentario = styled.p`
+    margin-top: 10em;
+    margin-left: 5%;
+    margin-right: 5%;
+    color: #FFFF;
+    font-size: 90%;
+`
+
+export const Nombre = styled.p`
+    margin-top: 15%;
+    margin-left: 5%;
+    margin-right: 5%;
+    color: #FFFF;
+    font-size: 70%;
+`
