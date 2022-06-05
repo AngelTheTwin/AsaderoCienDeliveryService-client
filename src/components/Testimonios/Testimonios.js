f3f0f895528ef73430f3e1d1d56d9c94d1d0ec5b
Angel de Jesús Sánchez Morales <angeldejesussanchezm@icloud.com>
2022-06-04 20:39:22 -0500

    Testimonios Optimizados

--- src/components/Testimonios/Testimonios.js
+++ src/components/Testimonios/Testimonios.js
@@ -1,9 +1,9 @@
 import React from "react"
-import {Tarjeta} from "./TestimoniosElements"
-import {Img} from "./TestimoniosElements"
-import {Comentario} from "./TestimoniosElements"
-import {Nombre} from "./TestimoniosElements"
-import {Title} from "./TestimoniosElements"
+import { Tarjeta } from "./TestimoniosElements"
+import { Img } from "./TestimoniosElements"
+import { Comentario } from "./TestimoniosElements"
+import { Nombre } from "./TestimoniosElements"
+import { Title } from "./TestimoniosElements"
 
 
 import imagenDaniel from "../../assets/Daniel.jpg"
@@ -24,44 +24,46 @@ export default function Testimonios() {
 
 	return (
 		<div id="testimonios">
-		<Title>Nuestra pasión, tu satisfacción...</Title>
-		<Swiper        
-			pagination={{dynamicBullets: true,}}
-        	modules={[Pagination]}>
-
-		  	<SwiperSlide>
-				<Tarjeta>
-					<Comentario>“Un lugar muy agradable, con muy buenos platillos. Variedad de cortes, ensaladas y hasta pizzas al horno. Hay también desayuno buffet. El personal es muy atento y amable, siempre al pendiente de los clientes. Definitivamente es recomendable”.</Comentario>
-					<Nombre>Daniel García Morales</Nombre>
-				</Tarjeta> 
-				<Img src={imagenDaniel}/>
-		  	</SwiperSlide>
-
-		  	<SwiperSlide>
-				<Tarjeta>
-					<Comentario>“Somos clientes asiduos, mis papás y yo, de la sucursal de El Dorado. El servicio es excelente, todo se encuentra en orden y siempre es un placer volver”.</Comentario>
-					<Nombre>Angel de Jesús Sánchez Morales</Nombre>
-					</Tarjeta> 
-				<Img src={imagenAngel}/>
-			</SwiperSlide>
-
-			<SwiperSlide>
-				<Tarjeta>
-					<Comentario>“Excelentes carnes, hamburguesas y el inmejorable trato en renta de sus salones”.</Comentario>
-					<Nombre>José Damián Mendoza Carmona</Nombre>
-					</Tarjeta> 
-				<Img src={imagenDamian}/>
-			</SwiperSlide>
-
-			<SwiperSlide>
-				<Tarjeta>
-					<Comentario>“Muy rico y buen servicio :D”.</Comentario>
-					<Nombre>Abner Jeffrey Tapia Cruz</Nombre>
-				</Tarjeta> 
-				<Img src={imagenAbner}/>
-			</SwiperSlide>
-
-		</Swiper>
-	  </div>
+			<Title>Nuestra pasión, tu satisfacción...</Title>
+			<Swiper
+				pagination={{ dynamicBullets: true, }}
+				modules={[Pagination]}
+			>
+				{testimonios.map(testimonio => {
+					return (
+						<SwiperSlide key={testimonio.nombre}>
+							<Tarjeta>
+								<Comentario> {testimonio.comentario} </Comentario>
+								<Nombre> {testimonio.nombre} </Nombre>
+							</Tarjeta>
+							<Img src={testimonio.imagen} />
+						</SwiperSlide>
+					)
+				})}
+			</Swiper>
+		</div>
 	)
 }
+
+const testimonios = [
+	{
+		nombre: 'Daniel García Morales',
+		comentario: '“Un lugar muy agradable, con muy buenos platillos. Variedad de cortes, ensaladas y hasta pizzas al horno. Hay también desayuno buffet. El personal es muy atento y amable, siempre al pendiente de los clientes. Definitivamente es recomendable”.',
+		imagen: imagenDaniel
+	},
+	{
+		nombre: 'Angel de Jesús Sánchez Morales',
+		comentario: '“Somos clientes asiduos, mis papás y yo, de la sucursal de El Dorado. El servicio es excelente, todo se encuentra en orden y siempre es un placer volver”.',
+		imagen: imagenAngel,
+	},
+	{
+		nombre: 'José Damián Mendoza Carmona',
+		comentario: '“Excelentes carnes, hamburguesas y el inmejorable trato en renta de sus salones”.',
+		imagen: imagenDamian
+	},
+	{
+		nombre: 'Abner Jeffrey Tapia Cruz',
+		comentario: '“Muy rico y buen servicio :D”.',
+		imagen: imagenAbner
+	},
+]
