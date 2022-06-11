57ac00e0184e1171d9548c939d5652b547fc5417
Daniel Morales <dagamo0605@gmail.com>
2022-06-11 18:50:58 -0500

    Covid 19 page

--- src/pages/Covid19/Covid19.js
+++ src/pages/Covid19/Covid19.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import Lottie from 'lottie-web'
 
 import {
 	Covid19Content,
@@ -6,10 +7,56 @@ import {
 	BackButton,
 	BackIcon,
 	Title,
+    AnimationContent,
+    MessageContent,
+    MessageTitle,
+    Subtitle,
+    Message,
+    AnimatedIcon
 } from './Covid19Elements'
 
 export const Covid19 = () => {
     
+    const earthAnimation = useRef(null)
+    const deliveryAnimation = useRef(null)
+    const covidAnimation = useRef(null)
+    const maskAnimation = useRef(null)
+
+    useEffect(() => {
+        Lottie.loadAnimation({
+            container: earthAnimation.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../assets/earth-mask.json')
+        })
+
+        Lottie.loadAnimation({
+            container: deliveryAnimation.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../assets/delivery-service.json')
+        })
+
+        Lottie.loadAnimation({
+            container: covidAnimation.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../assets/covid-19.json')
+        })
+
+        Lottie.loadAnimation({
+            container: maskAnimation.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../assets/delivery-mask.json')
+        })
+
+    }, [])
+
     return(
         <Covid19Content>
             <Navbar id="nav" >
@@ -19,6 +66,25 @@ export const Covid19 = () => {
                 <Title >Asadero Cien - COVID-19: Centro de seguridad</Title>
             </Navbar>
 
+            <AnimationContent ref={earthAnimation}/>
+
+            <MessageContent>
+                <MessageTitle>Protección a la comunidad durante la situación COVID-19</MessageTitle>
+
+                <Message>Trabajamos en estrecha colaboración con las autoridades de salud pública para brindar orientación sobre cómo protegerte y cómo proteger a los demás.</Message>
+            
+                <AnimatedIcon ref={deliveryAnimation}/>
+                <Subtitle>Cuando el pedido llegue a tu puerta</Subtitle>
+                <Message>Lávate las manos después de recibir tu pedido y antes de comer.</Message>
+
+                <AnimatedIcon ref={covidAnimation}/>
+                <Subtitle>Seguridad alimentaria general</Subtitle>
+                <Message>Compartimos las mejores prácticas de las autoridades de salud pública con nuestros restaurantes y socios repartidores</Message>
+
+                <AnimatedIcon ref={maskAnimation}/>
+                <Subtitle>Ayudamos a los socios repartidores</Subtitle>
+                <Message>Les incluimos el acceso a mascarillas y a asistencia financiera, de acuerdo con nuestra política de respuesta ante la situación de coronavirus.</Message>
+            </MessageContent>
 
         </Covid19Content>
 
--- src/pages/Covid19/Covid19Elements.js
+++ src/pages/Covid19/Covid19Elements.js
@@ -10,6 +10,9 @@ export const Covid19Content = styled.div`
 	width: 100%;
 	color: white;
 	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
 `
 
 export const Navbar = styled.nav`
@@ -43,4 +46,44 @@ export const BackIcon = styled(BiArrowBack)`
 
 export const Title = styled.h4`
 	margin: 0;
+`
+
+export const AnimationContent = styled.div`
+	width: 30%;
+
+	@media screen and (max-width: 768px) {
+        width: 60%;
+	}
+`
+
+export const AnimatedIcon = styled.div`
+	width: 20%;
+
+	@media screen and (max-width: 768px) {
+        width: 30%;
+	}
+`
+
+export const MessageContent = styled.div`
+	width: 65%;
+
+	@media screen and (max-width: 768px) {
+        width: 80%;
+	}
+`
+export const MessageTitle = styled.h1`
+	font-style: bold;
+`
+
+export const Subtitle = styled.h2`
+`
+
+export const Message = styled.p`
+	font-family: 'ABeeZee';
+	font-style: normal;
+	padding-bottom: 10%;
+
+	@media screen and (max-width: 768px) {
+        padding-bottom: 15%;
+	}
 `
